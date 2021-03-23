e79da172f67642d17d64de5b91bfe396192c4545
Daichi SEKI <daichi.a.seki@gmail.com>
2021-03-24 00:22:04 +0100

    Add suspend support in example

--- example/ESP32_ServoSwitch_control/ESP32_ServoSwitch_control.cpp
+++ example/ESP32_ServoSwitch_control/ESP32_ServoSwitch_control.cpp
@@ -1,13 +1,14 @@
 #include <Arduino.h>
 #include "ESP32_ServoSwitch.h"
 
-#define SERVOPIN_1 (26)     // GPIO 26 is used for PWM
-#define CHANNEL_SERVO_1 (0) // 0 - 15 can be used for PWM channel
-#define SWITCH_OFF_DEG (75) // Switch OFF at 75 deg
-#define SWITCH_ON_DEG (135) // Switch ON at 135 deg
+#define SERVOPIN_1 (26)        // GPIO 26 is used for PWM
+#define CHANNEL_SERVO_1 (0)    // 0 - 15 can be used for PWM channel
+#define SWITCH_OFF_DEG (75)    // Switch OFF at 75 deg
+#define SWITCH_ON_DEG (135)    // Switch ON at 135 deg
+#define SUSPEND_ENABLED (true) // Enable suspend support. (avoid shaking by detaching servo after few seconds)
 
 int toggle = 0;
-ESP32_ServoSwitch switch1(SERVOPIN_1, CHANNEL_SERVO_1, SWITCH_OFF_DEG, SWITCH_ON_DEG); // Construct Switch1 object
+ESP32_ServoSwitch switch1(SERVOPIN_1, CHANNEL_SERVO_1, SWITCH_OFF_DEG, SWITCH_ON_DEG, SUSPEND_ENABLED); // Construct Switch1 object
 // please see "ESP32_ServoSwitch.h", servo parameter is configuable
 
 void setup()
